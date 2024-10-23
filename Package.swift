d2c201340c4482953b56c3c34b703e00e490589c
manzick <manzurov.alex@yahoo.com>
2024-10-23 16:40:40 +0600

    Release 2.12.11

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DashlySDK",
-            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/2.12.11/CarrotSDK.xcframework.zip",
+            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/2.12.11/DashlySDK.xcframework.zip",
             checksum: "43db97f3905e02e0c3dbd2d4b962e3060c2fd9a1808af50e6babfaf77bc299ae"
         ),
     ]
