62757ba68c80ae522402127e20e29e81f7235af1
Alex M <manzurov.alex@yahoo.com>
2026-04-08 21:19:17 +0600

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DashlySDK",
-            url: "{URL}",
-            checksum: "{CHECKSUM}"
+            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/3.1.3/DashlySDK.xcframework.zip",
+            checksum: "52b86304358c61d42a34c2d844aa3e961f5baf6a905a3792f9ad1967df45c9a6"
         ),
     ]
 )
