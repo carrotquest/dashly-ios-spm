8b0b54011689d0bdaaaccca3c39da7d12a0fc9ab
Manzick <manzurov.alex@yahoo.com>
2026-05-25 23:10:12 +0600

    Improved UTM handling 3.1.5

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DashlySDK",
-            url: "{URL}",
-            checksum: "{CHECKSUM}"
+            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/3.1.5/DashlySDK.xcframework.zip",
+            checksum: "64acaa67f74a430a239806697001cf44f77b1adfef7e2ce59f5ac61e93a98aff"
         ),
     ]
 )
