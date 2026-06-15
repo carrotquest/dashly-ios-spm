6d704e3e360154a2d79a6e2ff4cb2c8fab408a81
Manzick <manzurov.alex@yahoo.com>
2026-06-15 16:46:10 +0600

    New UI

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DashlySDK",
-            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/3.1.5/DashlySDK.xcframework.zip",
-            checksum: "64acaa67f74a430a239806697001cf44f77b1adfef7e2ce59f5ac61e93a98aff"
+            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/3.2.0/DashlySDK.xcframework.zip",
+            checksum: "d23ff44fa42c8da5f91dfa137d8224b5c7809ba8650471a5b40ed6c2169bae0a"
         ),
     ]
 )
