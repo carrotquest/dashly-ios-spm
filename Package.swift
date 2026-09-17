44f9d560af94e5b0440012cdf2d3594a3dce832d
Manzick <manzurov.alex@yahoo.com>
2026-09-17 15:21:50 +0600

    Swift 6, No More Racing 3.3.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DashlySDK",
-            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/3.3.0/DashlySDK.xcframework.zip",
-            checksum: "a986c06c03f81d52e607ab69f301f5e531fa41d725214fed445e89158c1845ef"
+            url: "https://github.com/carrotquest/dashly-ios-spm/releases/download/3.3.1/DashlySDK.xcframework.zip",
+            checksum: "42cefebef74ec56e6d5d555119a2298353f9833f398da56b6d3e06f5fe9670e5"
         ),
     ]
 )
